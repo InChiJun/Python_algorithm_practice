59f36a22db8481b79e0d72ee4ae51b9c49a32e2b
Deniel_dev <clwnssla1@bible.ac.kr>
2022-09-05 09:23:23 +0900

    이미지 딥러닝 3번 문제 22-9/5

--- exam003.py
+++ exam003.py
@@ -1,5 +1,4 @@
 N, M = map(int, input().split()); # N, M 할당
-arr = [0];
 arr = list(map(int, input().split())); # arr 배열 받기
 
 Sarr = [];
@@ -10,4 +9,5 @@ for i in arr: # 합배열 만들기
 
 for k in range(M):
     i, j = map(int, input().split());
-    print(Sarr[j + 1] - Sarr[i - 1]);
+    if (i > 1): print(Sarr[j - 1] - Sarr[i - 2]);
+    else: print(Sarr[j - 1]);
