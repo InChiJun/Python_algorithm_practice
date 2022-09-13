efc89fdd8b439b4ad5ec4c9b9afbe31485e23c2c
Daniel <clwnssla1@bible.ac.kr>
2022-09-13 23:43:31 +0900

    4번 문제_by 이삭학우님 22-9/13

--- 인치준 04.py
+++ 인치준 04.py
@@ -1,9 +1,15 @@
-def prefixSum(N, M, str0, str1, str2, str3, ask):
-    arr = [];
-    for i in range(N):
-        arr.append(str(i))    
+def remainderSum(N, M):
+    arr = [list(map(int, input().split())) for i in range(N)];
+    dp = [[0] * (N + 1) for i in range(N + 1)];
 
+    for i in range(1, N + 1):
+        for j in range(1, N + 1):
+            dp[i][j] = dp[i][j - 1] + dp[i - 1][j] - dp[i - 1][j - 1] + arr[i - 1][j - 1];
 
-prefixSum(4, 3, "1 2 3 4", "2 3 4 5", "3 4 5 6", "4 5 6 7", "2 2 3 4");
-prefixSum(4, 3, "1 2 3 4", "2 3 4 5", "3 4 5 6", "4 5 6 7", "3 4 3 4");
-prefixSum(4, 3, "1 2 3 4", "2 3 4 5", "3 4 5 6", "4 5 6 7", "1 1 4 4");
+    for i in range(M):
+        x1, y1, x2, y2 = map(int, input().split());
+
+        result = dp[x2][y2] - dp[x2][y1 - 1] - dp[x1 - 1][y2] + dp[x1 - 1][y1 - 1];
+        print(result);
+
+remainderSum(4, 3);
