28e827cce186bb47d35e13c36aab8d3424198a97
Daniel <clwnssla1@bible.ac.kr>
2022-09-30 00:19:08 +0900

    16번 문제

--- 인치준 16.py
+++ 인치준 16.py
@@ -4,6 +4,14 @@ N = int(stdin.readline().rstrip())
 A = []
 
 for i in range(N):
-    A[i] = int(stdin.readline().rstrip())
+    A.append((int(stdin.readline().rstrip()), i))
 
-A.sort()
+sortedA = sorted(A)
+max = 0
+
+for j in range(N):
+    index = sortedA[j][1] - j
+    if (index > max):
+        max = index
+
+print(max + 1)
