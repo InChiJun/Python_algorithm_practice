308138ff3af9ba37a2a03ba6aad4a514919de545
Daniel_Home <clwnssla1@bible.ac.kr>
2022-11-18 00:24:04 +0900

    no message

--- 인치준 32.py
+++ 인치준 32.py
@@ -1,3 +1,5 @@
+# 출처 : https://www.acmicpc.net/problem/11047
+
 from sys import stdin
 
 input = stdin.readline
@@ -6,10 +8,10 @@ n, k = map(int, input().split())
 cnt = 0
 A = []
 for i in range(n):
-    A.append(input().rstrip())
+    A.append(int(input().rstrip()))
 
 for i in range(len(A) - 1, -1, -1):
-    if (k / A[i]) > 0:
+    if (k // A[i]) > 0:
         cnt += k // A[i]
         k %= A[i]
 
