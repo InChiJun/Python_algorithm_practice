7bb9eff7bb61027e8c94eae963f03c424e097a2c
Korean's_com <clwnssla1@bible.ac.kr>
2022-12-08 09:50:04 +0900

    36번 문제

---
+++ 인치준 36.py
@@ -0,0 +1,16 @@
+from sys import stdin
+
+input = stdin.readline
+
+a = input().split('-')
+num = []
+for i in a:
+    cnt = 0
+    s = i.split('+')
+    for j in s:
+        cnt += int(j)
+    num.append(cnt)
+n = num[0]
+for i in range(1, len(num)):
+    n -= num[i]
+print(n)
